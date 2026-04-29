9c67801884cfd2e1bee2b0c0324cc7bc36d8c0e7
estrailer <104133419+estrailer@users.noreply.github.com>
2026-04-29 10:09:48 +0200

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,77 @@
+const datos = {
+  1: {
+    nombre: "Vitrina 1",
+    apartados: {
+      1: {
+        nombre: "Invertebrados",
+        imagen: "img/invertebrados.jpg",
+        cajones: {
+          1: { nombre: "Moluscos", animales: { 1: { nombre: "Caracol", info: "..." } } },
+          2: { nombre: "Artrópodos", animales: { 1: { nombre: "Araña", info: "..." } } },
+          3: { nombre: "Insectos", animales: { 1: { nombre: "Mariposa", info: "..." } } },
+          4: { nombre: "Crustáceos", animales: { 1: { nombre: "Cangrejo", info: "..." } } },
+          5: { nombre: "Otros", animales: { 1: { nombre: "Esponja", info: "..." } } }
+        }
+      },
+      2: {
+        nombre: "Anfibios",
+        imagen: "img/anfibios.jpg",
+        cajones: {
+          1: { nombre: "Ranas", animales: { 1: { nombre: "Rana verde", info: "..." } } },
+          2: { nombre: "Sapos", animales: { 1: { nombre: "Sapo común", info: "..." } } },
+          3: { nombre: "Salamandras", animales: { 1: { nombre: "Salamandra", info: "..." } } },
+          4: { nombre: "Tritones", animales: { 1: { nombre: "Tritón", info: "..." } } },
+          5: { nombre: "Otros", animales: { 1: { nombre: "Ajolote", info: "..." } } }
+        }
+      }
+    }
+  },
+
+  2: {
+    nombre: "Vitrina 2",
+    apartados: {
+      3: {
+        nombre: "Peces",
+        imagen: "img/peces.jpg",
+        cajones: { 1:{nombre:"1"},2:{nombre:"2"},3:{nombre:"3"},4:{nombre:"4"},5:{nombre:"5"} }
+      },
+      4: {
+        nombre: "Aves",
+        imagen: "img/aves.jpg",
+        cajones: { 1:{nombre:"1"},2:{nombre:"2"},3:{nombre:"3"},4:{nombre:"4"},5:{nombre:"5"} }
+      }
+    }
+  },
+
+  3: {
+    nombre: "Vitrina 3",
+    apartados: {
+      5: {
+        nombre: "Reptiles",
+        imagen: "img/reptiles.jpg",
+        cajones: { 1:{},2:{},3:{},4:{},5:{} }
+      },
+      6: {
+        nombre: "Mamíferos",
+        imagen: "img/mamiferos.jpg",
+        cajones: { 1:{},2:{},3:{},4:{},5:{} }
+      }
+    }
+  },
+
+  4: {
+    nombre: "Vitrina 4",
+    apartados: {
+      7: {
+        nombre: "Insectos",
+        imagen: "img/insectos.jpg",
+        cajones: { 1:{},2:{},3:{},4:{},5:{} }
+      },
+      8: {
+        nombre: "Otros",
+        imagen: "img/otros.jpg",
+        cajones: { 1:{},2:{},3:{},4:{},5:{} }
+      }
+    }
+  }
+};
