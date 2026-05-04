002aa110f71e700bb75eabd6c91abfc36b015660
estrailer <104133419+estrailer@users.noreply.github.com>
2026-05-04 09:45:51 +0200

    Update datos.js

--- datos.js
+++ datos.js
@@ -6,9 +6,26 @@ const datos = {
         nombre: "Invertebrados",
         imagen: "img/invertebrados.jpg",
         cajones: {
-          1: { nombre: "Moluscos", animales: { 1: { nombre: "Caracol", info: "...", imagen: "img/caracol.png" } } },
-          2: { nombre: "Artrópodos", animales: { 1: { nombre: "Araña", info: "..." } } },
-          3: { nombre: "Insectos", animales: { 1: { nombre: "Mariposa", info: "..." } } },
+          1: { nombre: "Moluscos", animales: { 1: { nombre: "Caracol", 
+            info: `Un caracol es un nombre común para una especie de molusco. El término se usa para un gasterópodo con una carcasa enrollada. 
+              Sus registros fósiles se extienden hacia el período carbonífero. El término 'caracol' también se usa a veces para gasterópodos acuáticos en forma de caracol, 
+              que generalmente tienen branquias. En realidad, la mayoría de las especies de caracoles son caracoles marinos. Hay más especies de ellas, y son mucho mayores en el número. 
+              También se pueden encontrar muchos tipos de caracoles en hábitats de agua dulce.La mayoría de los caracoles y las babosas son herbívoros. Los caracoles y babosas acuáticas suelen ser omnívoros o carnívoros depredadores.`, 
+            imagen: "img/caracol.png" } } },
+
+          2: { nombre: "Artrópodos", animales: { 1: { nombre: "Araña", 
+            info: `Las arañas están en todo nuestro planeta tierra, desde las selvas, pasando por los desiertos y polos hasta llegar a las grandes ciudades. 
+              Las arañas son seres que poseen herramientas especiales para poder sobrevivir. Por eso es que ellas son tejedoras expertas, producen una sustancia especial que les permite 
+              hacer telarañas grandes o pequeñas, sea para cazar sus presas, transportarse o para que sus huevos estén protegidos dentro de un saco que ella misma fabrica. 
+              Las glándulas que están situadas en la parte superior de su abdomen controlan el líquido que se solidifica al estar en contacto con el aire convirtiéndose en la fibra pegajosa 
+              con la que fabrica su tela. Las arañas tienen un tamaño pequeño, pero también las hay grandes. El color depende de la especie. Por ejemplo las exóticas, con sus colores son 
+              brillantes; otras lo cambian como camuflaje de depredadores o dependiendo el lugar en el que se encuentren. La mayoría de arañas tienen un promedio de vida de un año. 
+              Sin embargo, hay especies que pueden vivir alrededor de 8 o 10 años. Las tarántulas por ejemplo, pueden alcanzar los 30 años.`, 
+            imagen: "img/arana.png" } } },
+
+          3: { nombre: "Insectos", animales: { 1: { nombre: "Mariposa", 
+            info: ``, 
+            imagen: "img/" } } },
           4: { nombre: "Crustáceos", animales: { 1: { nombre: "Cangrejo", info: "..." } } },
           5: { nombre: "Otros", animales: { 1: { nombre: "Esponja", info: "..." } } }
         }
